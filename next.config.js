61d770667bba287467367e1f7ba79e65032131dc
resmith <36131619+samiti3d@users.noreply.github.com>
2023-10-16 19:22:32 +0700

    next config output export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = { 
+  output: 'export',
+  reactStrictMode: true, 
+  swcMinify: true 
+}
 
 module.exports = withContentlayer(nextConfig)
